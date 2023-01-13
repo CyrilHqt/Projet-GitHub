d80a321e098d511d0bf371ccadbf60cb222438ef
Yves Boulinguez <yves.boulinguez@grow-your-business.fr>
2023-01-13 14:29:52 +0100

    feat: added test function to call api

--- assets/js/meteo.js
+++ assets/js/meteo.js
@@ -1,1 +1,23 @@
 const API_KEY = 'be4afa03f168cfa15bf1a322864f9c1e';
+
+async function getInfoAPi(){
+
+    try {
+
+        const request = await fetch(`https:api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_KEY}`);
+        console.log('request', request);
+
+        if (!request.ok) {
+            throw new Error(`Erreur: ${request.status}`)
+        }
+
+        const data = await request.json();
+        console.log('data', data);
+
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
+window.addEventListener('load', getInfoAPi)
