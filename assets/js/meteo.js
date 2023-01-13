dea74f0573a211c6b72a7ff57c24487d496e93c8
Yves Boulinguez <yves.boulinguez@grow-your-business.fr>
2023-01-13 14:38:33 +0100

    feat: prepared style code for show the loader

--- assets/js/meteo.js
+++ assets/js/meteo.js
@@ -1,23 +1,20 @@
-const API_KEY = 'be4afa03f168cfa15bf1a322864f9c1e';
+const API_KEY = "be4afa03f168cfa15bf1a322864f9c1e";
 
-async function getInfoAPi(){
+async function getInfoAPi() {
+  try {
+    const request = await fetch(
+      `https:api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_KEY}`
+    );
 
-    try {
-
-        const request = await fetch(`https:api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_KEY}`);
-        console.log('request', request);
-
-        if (!request.ok) {
-            throw new Error(`Erreur: ${request.status}`)
-        }
-
-        const data = await request.json();
-        console.log('data', data);
-
-    } catch (error) {
-        console.log(error)
-
+    if (!request.ok) {
+      throw new Error(`Erreur: ${request.status}`);
     }
+
+    const data = await request.json();
+    console.log("data", data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-window.addEventListener('load', getInfoAPi)
+window.addEventListener("load", getInfoAPi);
