f255bcd6a04d43980a4fa1f09e3092bc005b55b8
Yves Boulinguez <yves.boulinguez@grow-your-business.fr>
2023-01-13 14:48:13 +0100

    feat: added constantes, added get latitude and longitude in load document

--- assets/js/meteo.js
+++ assets/js/meteo.js
@@ -1,5 +1,31 @@
 const API_KEY = "be4afa03f168cfa15bf1a322864f9c1e";
 
+const currentWeather = document.querySelector("img");
+const currentDegre = document.querySelector(".degre");
+const currentPosition = document.querySelector(".localisation");
+const currentHour = document.getElementsByClassName("current-hour");
+const currentTemp = document.getElementsByClassName("current-temp");
+const felt = document.querySelector('.felt')
+const loader = document.querySelector('.dot-group');
+const responseAPI = document.querySelector('.top');
+
+let currentLatitude;
+let currentLongitude;
+
+window.addEventListener("load", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            currentLatitude = position.coords.latitude;
+            currentLongitude = position.coords.longitude;
+            console.log(
+                currentLatitude,
+                currentLongitude,
+            );
+            // getCurrentPositionUser()
+        });
+    }
+});
+
 async function getInfoAPi() {
   try {
     const request = await fetch(
